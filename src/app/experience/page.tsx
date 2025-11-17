e81fdcc631ccf1e20a82c0f177890ca81af0728a
ifanPCS <ifan.alriansyah@pcsindonesia.co.id>
2025-11-17 11:13:18 +0700

    adjst(experiencePage): add experience content

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -6,6 +6,7 @@ const data = [
   {
     title: 'KerjaYuk!',
     subtitle: 'Human Resource Application',
+    url: 'https://kerjayuk-hr.pcsindonesia.co.id/login',
     date: 'PCS Payment - (Mar 2025 - Present)',
     tech: [
       'Javascript',
@@ -22,6 +23,7 @@ const data = [
   {
     title: 'Kopra by Mandiri',
     subtitle: 'Portal Application',
+    url: 'https://koprabymandiri.com',
     date: 'Lawencon International - (Sep 2022 - Feb 2025)',
     tech: [
       'Javascript',
@@ -38,11 +40,20 @@ const data = [
     img: ['/kopra.png', '/kopra-2.png'],
     description: `Kopra by Mandiri is a web-based digital platform that serves as an online portal, consolidating monitoring, and access services for the financial and transactional activities of Customers.`,
   },
+  {
+    title: 'CIMB Biztrip',
+    subtitle: 'Booking Flight and Hotel Application',
+    url: '',
+    date: 'Jojonomic - (Dec 2021 - Sep 2022)',
+    tech: ['Javascript', 'Vue', 'Jquery', 'Bootstrap', 'Jenkins', 'Docker'],
+    img: [],
+    description: `Biztrip is an internal application from CIMB Bank that employees use to book airline tickets and hotels, eliminating the need for personal funds and reimbursements. The app is integrated with the Traveloka API.`,
+  },
 ];
 
 export default function Experience() {
   return (
-    <div className='bg-[#1C1C1C] h-[100vh] p-4 md:p-10'>
+    <div className='bg-[#1C1C1C] h-full p-4 md:p-10'>
       <Stepper
         initialStep={1}
         onStepChange={(step) => {
@@ -58,16 +69,28 @@ export default function Experience() {
               <h1 className='text-[40px] md:text-[80px] font-semibold text-white'>
                 {item.title}
               </h1>
-              <p className='text-[15px] md:text-[30px] font-light text-white'>
+              {item.url ? (
+                <a
+                  href={item.url}
+                  className='text-sm text-white italic'
+                  target='_blank'
+                >
+                  {item.url}
+                </a>
+              ) : (
+                <p className='text-sm text-white italic'>Private App</p>
+              )}
+              <p className='text-[15px] md:text-[30px] font-light text-white mt-4'>
                 {item.subtitle}
               </p>
               <p className='text-white italic'>{item.date}</p>
-              <div className='flex flex-wrap justify-center md:justify-start gap-2 mt-4'>
+              <div className='flex flex-wrap justify-center md:justify-start gap-4 mt-4'>
                 {item.tech.map((item, index) => {
                   return (
                     <div
                       key={index}
-                      className='text-xs md:text-base flex items-center justify-center bg-[#9FFFA9] px-3 py-[2px] rounded-full'
+                      className='cursor-pointer bg-[#9FFFA9] text-xs md:text-base flex items-center justify-center px-3 py-[2px] rounded-full transition-all duration-300 ease-out hover:bg-[#0057FF] hover:text-white hover:scale-110
+                    '
                     >
                       {item}
                     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang='en' className={poppins.variable}>
       <body className={`${poppins.className} text-white flex min-h-screen`}>
         <Sidebar />
-        <main className='flex-1 h-screen overflow-y-auto bg-white text-black custom-scrollbar'>
+        <main className='flex-1 h-screen overflow-y-auto bg-[#1C1C1C] text-black custom-scrollbar'>
           {children}
         </main>
       </body>
